9fef8314f3bfd35c0792cc70bafe3ce37f5ff585
Prime <prime@Primes-MacBook-Pro.local>
2019-10-12 14:12:08 -0500

    form building

--- client/src/components/DriverView/DriverView.js
+++ client/src/components/DriverView/DriverView.js
@@ -6,18 +6,45 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 
 class DriverView extends Component {
-    state = {
-        heading: 'Driver View',
-        productStats: {
-            par: '',
-            lastPar: '',
-            sold: '',
-            shrink: '',
-            added: '',
-        },
-        productStats: [],
+    constructor() {
 
-    };
+        this.state = {
+            formControls: {
+                par: {
+                    value: ''
+                },
+                lastPar: {
+                    value: ''
+                },
+                sold: {
+                    value: ''
+                },
+                shrink: {
+                    value: ''
+                },
+                added: {
+                    value: ''
+                }
+            }
+        }
+
+    }
+    changeHandler = event => {
+
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            formControls: {
+                [name]: {
+                    ...this.state.formControls[name],
+                    value
+                }
+            }
+        });
+    }
+
+
     render() {
         return (
             <div>
@@ -27,6 +54,37 @@ class DriverView extends Component {
                 </h2>
                 <p>Your ID is: {props.store.user.id}</p>
 
+                <form>
+                    <input type="text"
+                        name="par"
+                        value={this.state.formControls.par.value}
+                        onChange={this.changeHandler}
+                    />
+
+                    <input type="text"
+                        name="last par"
+                        value={this.state.formControls.lastPar.value}
+                        onChange={this.changeHandler}
+                    />
+
+                    <input type="text"
+                        name="sold"
+                        value={this.state.formControls.sold.value}
+                        onChange={this.changeHandler}
+                    />
+
+                    <input type="text"
+                        name="shrink"
+                        value={this.state.formControls.shrink.value}
+                        onChange={this.changeHandler}
+                    />
+
+                    <input type="text"
+                        name="added"
+                        value={this.state.formControls.added.value}
+                        onChange={this.changeHandler}
+                    />
+                </form>
             </div>
         );
     }
@@ -35,4 +93,4 @@ class DriverView extends Component {
 
 
 
-export default DriverView;
+export default connect(mapStoreToProps)(DriverView);
