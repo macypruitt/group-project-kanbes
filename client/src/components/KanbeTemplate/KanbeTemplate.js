47ca52bcd8c222591f6c93c3e50d76b0011f29a6
Shanice Gipson <shanicegipson13@gmail.com>
2019-10-14 10:46:43 -0500

    drawer feature working

--- client/src/components/KanbeTemplate/KanbeTemplate.js
+++ client/src/components/KanbeTemplate/KanbeTemplate.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import clsx from 'clsx';
 
@@ -10,137 +10,140 @@ import {
   IconButton,
   Drawer,
   Divider,
-  Hidden
+  // Hidden
 } from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
 import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { lightGreen } from '@material-ui/core/colors';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 
 
 const drawerWidth = 240;
 
-const styles = (theme: Theme) => 
+const styles = (theme: Theme) =>
   createStyles({
-  root:{
-    display: 'flex',
-    // color: lightGreen[300]
-  },
-  appBar:{
-    colorPrimary: lightGreen[300],
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  }, 
-  appBarShift: {
-    width: `calc(100% -${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-});
+    root: {
+      display: 'flex',
+      // color: lightGreen[300]
+    },
+    appBar: {
+      colorPrimary: lightGreen[300],
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% -${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  });
 
 class KanbeTemplate extends Component {
   state = {
-    drawerOpen: false
+    open: false
   }
 
-  handleDrawerToggle = () => {
-    this.setState({drawerOpen: !this.state.drawerOpen})
+  handleDrawerOpen = () => {
+    this.setState({ open: true })
   }
 
-    render() {
-      const drawer = (
-        <div>
-          <div className={this.props.classes.toolbar} />
-          Hello
-          <Divider />
-          Test
-          <Divider />
-        </div>
-      )
-        return (
-            <div className={this.props.classes.root}>
-                <CssBaseline />
-                <AppBar
-                    position="fixed"
-                    className={clsx(this.props.classes.appBar && 
-                        this.props.classes.appBarShift)}
-                >
-                    <Toolbar>
-                        <IconButton
-                            color='primary'
-                            aria-label="open drawer"
-                            onClick={this.handleDrawerOpen}
-                            edge="start"
-                            className={clsx(this.props.classes.menuButton && 
-                                this.props.classes.hide)}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" noWrap>
-                        Kanbe's Market
+  handleDrawerClose = () => {
+    this.setState({ open: false })
+  }
+
+  render() {
+    
+    return (
+      <div className={this.props.classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(this.props.classes.appBar &&
+            this.props.classes.appBarShift)}
+        >
+          <Toolbar>
+            <IconButton
+              color='primary'
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              edge="start"
+              // className={clsx(this.props.classes.menuButton &&
+              //   this.props.classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Kanbe's Market
                         </Typography>
-                    </Toolbar>
-                </AppBar>
-                <nav className={this.props.classes.drawer} aria-label="mailbox folders">
-                  {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                  <Hidden smUp implementation="css">
-                    <Drawer 
-                      className={this.props.classes.drawer}
-                      variant="persistent"
-                      anchor={'left'}
-                      open={this.state.drawerOpen}
-                      onClose={this.handleDrawerToggle}
-                      classes={{
-                        paper: this.props.classes.drawerPaper,
-                      }}
-                      ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                      }}
-                    >
-                      {drawer}
-                    </Drawer>
-                  </Hidden>
-                  <Hidden xsDown implementation="css">
-                    <Drawer
+          </Toolbar>
+        </AppBar>
+        {/* <nav className={this.props.classes.drawer} aria-label="mailbox folders"> */}
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            className={this.props.classes.drawer}
+            variant="persistent"
+            anchor={'left'}
+            open={this.state.open}
+            classes={{
+              paper: this.props.classes.drawerPaper,
+            }}
+          >
+            <div className={this.props.classes.drawerHeader}>
+              <IconButton onClick={this.handleDrawerClose}>
+                {/* {this.props.theme.direction === 'ltr' ? 
+                <ChevronLeftIcon /> :  */}
+                <ChevronRightIcon />
+                {/* } */}
+              </IconButton>
+            </div>
+            <Divider />
+            {/* {drawer} */}
+          </Drawer>
+          {/* <Drawer
                       classes={{
                         paper: this.props.classes.drawerPaper,
                       }}
@@ -148,17 +151,15 @@ class KanbeTemplate extends Component {
                       open
                     >
                       {drawer}
-                    </Drawer>
-                   </Hidden>
-                </nav>
-                <main className={this.props.classes.content}>
-                  <div className={this.props.classes.toolbar} />
-                  <h1>Template!</h1>
-                </main>
-            </div>
-        );
-    }
-    
+                    </Drawer> */}
+        <main className={this.props.classes.content}>
+          <div className={this.props.classes.toolbar} />
+          <h1>Template!</h1>
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default withStyles(styles)(KanbeTemplate);
