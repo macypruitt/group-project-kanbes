1f72f47bd5b63fef58844a5afaba7e41e51bd6cb
Prime <prime@Primes-MacBook-Pro.local>
2019-10-16 15:59:58 -0500

    creating framework for Manager page

--- client/src/components/ManagerPage/ManagerPage.js
+++ client/src/components/ManagerPage/ManagerPage.js
@@ -6,8 +6,49 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class ManagerPage extends Component {
     state = {
         heading: 'Manager View',
+        isEditable: this.props.editable || false,
+        isAddable: this.props.addable || false,
+        item: {}
     };
 
+    clickEdit = (event) => {
+        this.setState({
+            ...this.state,
+            isEditable: !this.state.isEditable,
+            item: this.props.item
+        }, ()=> {
+            console.log(this.state)
+        });
+    }
+
+    handleChangeInputText(event, dataKey){
+        const fieldValue = event.target.value;
+        this.setState({
+            ...this.state,
+            item: {
+                ...this.state.item,
+                [dataKey]:fieldValue
+            }
+        });
+        console.log(this.state);
+    }
+
+    clickSave = (event) => {
+        this.setState({
+            isEditable: !this.state.isEditable
+        })
+        console.log(this.state);
+    }
+
+    clickAdd = (event) => {
+        this.props.clickAddStore();
+this.setState({
+    isEditable: !this.state.isEditable,
+    isAddable: !this.state.isAddable
+
+});
+console.log(this.state)
+    } 
     render() {
         return (
             <div>
