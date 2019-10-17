ddd10c48542d274bb19f9615dc5024c5bb733174
Luke Becker <beckerluke1@gmail.com>
2019-10-17 10:10:20 -0500

    Created saga to get store inventory from server based on last modified. Created store inventory reducer to store individual store inventory data

--- server/routes/storeInventory.router.ts
+++ server/routes/storeInventory.router.ts
@@ -22,7 +22,7 @@ router.get('/:id', (req: Request, res: Response, next: express.NextFunction): vo
     pool.query(queryText, [storeId])
         .then((result) => {
         console.log(result.rows)
-        res.send(result.rows[0].name)})
+        res.send(result.rows)})
         .catch((err) => {
             console.log(`Error in GET store inventory: ${err}`);
             res.sendStatus(500)
