00503a8f54f558e5a987fd540914bd97343816d1
Ellen Saylor <ellensaylor8@gmail.com>
2019-10-18 13:36:11 -0500

    updated supplier name references in front and back end

--- client/src/components/AdminPage/Tables/SupplierTableRow.js
+++ client/src/components/AdminPage/Tables/SupplierTableRow.js
@@ -101,17 +101,17 @@ class SupplierTableRow extends Component {
     render() {
         const { classes, theme } = this.props;
         ////row data is passed to this component through props from SupplierTable.js
-        let name = this.props.item.supplier_name;
+        let supplier_name = this.props.item.supplier_name;
         let contact_name = this.props.item.contact_name;
         let contact_number = this.props.item.contact_number;
         let address = this.props.item.address;
         let editOrSaveButton = <Button className={classes.buttonPositive} onClick={this.clickEdit}>Edit</Button>
 
         if(this.state.isEditable){
-            name = <Input 
+            supplier_name = <Input 
                     className="row-input" 
-                    placeholder={name}
-                    onChange={(event) => this.handleChangeInputText(event, 'name')}
+                    placeholder={supplier_name}
+                    onChange={(event) => this.handleChangeInputText(event, 'supplier_name')}
                      />
             contact_name = <Input 
                     className="row-input" 
@@ -139,7 +139,7 @@ class SupplierTableRow extends Component {
 
         return (
             <tr>
-                <td>{name}</td>
+                <td>{supplier_name}</td>
                 <td>{contact_name}</td>
                 <td>{contact_number}</td>
                 <td>{address}</td>
