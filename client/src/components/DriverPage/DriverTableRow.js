55ac8c6479920ee8066e656237437194dc544e5d
Ellen Saylor <ellensaylor8@gmail.com>
2019-10-19 11:36:59 -0500

    added current timestamp to populate on table edit

--- client/src/components/DriverPage/DriverTableRow.js
+++ client/src/components/DriverPage/DriverTableRow.js
@@ -15,7 +15,7 @@ class DriverTableRow extends Component {
             ...this.state,
             isEditable: !this.state.isEditable,
             item: this.props.item
-        }, ()=>{
+        }, () => {
             console.log(this.state)
         })
     }
@@ -26,7 +26,8 @@ class DriverTableRow extends Component {
             ...this.state,
             item: {
                 ...this.state.item,
-                [dataKey]:fieldValue}
+                [dataKey]: fieldValue
+            }
         })
         console.log(this.state);
     }
@@ -37,7 +38,7 @@ class DriverTableRow extends Component {
         })
         console.log(this.state)
         ////WILL BE SENT TO DATABASE ONCE CONNECTED TO SERVER
-        this.props.dispatch({type: "ADD_OUTGOING_STORE", payload: this.state.item})
+        this.props.dispatch({ type: "ADD_OUTGOING_STORE", payload: this.state.item })
         // this.props.dispatch({ type: 'FETCH_STORE_INVENTORY', payload: id })
         // this.props.history.push(`/driver/${id}`);
     }
@@ -51,93 +52,99 @@ class DriverTableRow extends Component {
         console.log(this.state)
         ////WILL BE POSTED TO DATABASE ONCE CONNECTED TO SERVER
     }
-    
+
     render() {
         ////row data is passed to this component via props
         let product_name = this.props.item.product_name;
         let product_sub_type = this.props.item.product_sub_type;
         let standard_par = this.props.item.standard_par;
         let last_par = this.props.item.last_par;
-        let sold;
-        let shrink;
-        let notes;
+        let sold = this.props.item.sold_product_count;
+        let shrink = this.props.item.shrink_product_count;
+        let notes = this.props.item.notes;
+        let lastmodified = this.props.item.last_modified;
         let editOrSaveButton = <button onClick={this.clickEdit}>New Entry</button>;
+        var today = new Date();
+        var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+        var time = "T" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
         ////if Edit button is clicked, text inputs appear and Edit button becomes Save button
-        if(this.state.isEditable){
+        if (this.state.isEditable) {
             // product_name = <input 
             //         className="row-input" 
             //         placeholder={product_name}
             //         onChange={(event) => this.handleChangeInputText(event, 'product_name')}
             //          />
             // product_sub_type = <input 
-           
+
             //         className="row-input" 
             //         placeholder={product_sub_type}
             //         onChange={(event) => this.handleChangeInputText(event, 'product_sub_type')}
             //          />
-            standard_par = <input 
-                    type="tel"
-                    pattern="[0-9]*"
-                    className="row-input" 
-                    placeholder={standard_par}
-                    onChange={(event) => this.handleChangeInputText(event, 'standard_par')}
-                     />
-            last_par = <input 
-                    type="tel"
-                    pattern="[0-9]*"
-                    className="row-input" 
-                    placeholder={last_par}
-                    onChange={(event) => this.handleChangeInputText(event, 'last_par')}
-                     />
-            sold = <input 
-                    type="tel"
-                    pattern="[0-9]*"
-                    className="row-input" 
-                    onChange={(event) => this.handleChangeInputText(event, 'sold_product_count')}
-                     />
-            shrink = <input 
-                    type="tel"
-                    pattern="[0-9]*"
-                    className="row-input" 
-                    onChange={(event) => this.handleChangeInputText(event, 'shrink_product_count')}
-                     />
-            notes = <input 
-                    type="text"
-                    className="row-input" 
-                    onChange={(event) => this.handleChangeInputText(event, 'notes')}
-                     />
+            standard_par = <input
+                type="tel"
+                pattern="[0-9]*"
+                className="row-input"
+                placeholder={standard_par}
+                onChange={(event) => this.handleChangeInputText(event, 'standard_par')}
+            />
+            last_par = <input
+                type="tel"
+                pattern="[0-9]*"
+                className="row-input"
+                placeholder={last_par}
+                onChange={(event) => this.handleChangeInputText(event, 'last_par')}
+            />
+            sold = <input
+                type="tel"
+                pattern="[0-9]*"
+                className="row-input"
+                onChange={(event) => this.handleChangeInputText(event, 'sold_product_count')}
+            />
+            shrink = <input
+                type="tel"
+                pattern="[0-9]*"
+                className="row-input"
+                onChange={(event) => this.handleChangeInputText(event, 'shrink_product_count')}
+            />
+            notes = <input
+                type="text"
+                className="row-input"
+                onChange={(event) => this.handleChangeInputText(event, 'notes')}
+            />
+            lastmodified = date + time
             editOrSaveButton = <button data-id={this.props.item.id} onClick={this.clickSaveEntry}>Save New Entry</button>
         }
 
-         ////if 'Add Store' button is clicked, Edit changes to Add
-         if(this.state.isAddable){
+        ////if 'Add Store' button is clicked, Edit changes to Add
+        if (this.state.isAddable) {
             editOrSaveButton = <button data-id={this.props.item.id} onClick={this.clickAddEntry}>Add Entry</button>
-            product_name = <input 
-            className="row-input" 
-            placeholder={product_name}
-            onChange={(event) => this.handleChangeInputText(event, 'product_name')}
-             />
-    product_sub_type = <input 
-   
-            className="row-input" 
-            placeholder={product_sub_type}
-            onChange={(event) => this.handleChangeInputText(event, 'product_sub_type')}
-             />
+            product_name = <input
+                className="row-input"
+                placeholder={product_name}
+                onChange={(event) => this.handleChangeInputText(event, 'product_name')}
+            />
+            product_sub_type = <input
+
+                className="row-input"
+                placeholder={product_sub_type}
+                onChange={(event) => this.handleChangeInputText(event, 'product_sub_type')}
+            />
         }
 
         return (
-            
+
                 <tr id={this.props.key}>
-                    <td>{product_name}</td>
-                    <td>{product_sub_type}</td>
-                    <td>{standard_par}</td>
-                    <td>{last_par}</td>
-                    <td>{sold}</td>
-                    <td>{shrink}</td>
-                    <td>{notes}</td>
-                    <td>{editOrSaveButton}</td>
-                </tr>
+                <td>{product_name}</td>
+                <td>{product_sub_type}</td>
+                <td>{standard_par}</td>
+                <td>{last_par}</td>
+                <td>{sold}</td>
+                <td>{shrink}</td>
+                <td>{notes}</td>
+                <td>{lastmodified}</td>
+                <td>{editOrSaveButton}</td>
+            </tr>
         );
     }
 }
