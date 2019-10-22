ccf59fd32f8eef612c5579507291c32a115f15c3
Ellen Saylor <ellensaylor8@gmail.com>
2019-10-22 16:30:00 -0500

    deleted old post route to inventory table

--- server/routes/warehouseInventory.router.ts
+++ server/routes/warehouseInventory.router.ts
@@ -5,34 +5,6 @@ import pool from '../modules/pool';
 const router: express.Router = express.Router();
 
 /**
- * POST route for specific store inventory into incoming store
- */
-// router.post('/incoming_store', (req: Request, res: Response, next: express.NextFunction): void => {
-
-//   const user_id: string = req.body.user_id;
-//   const product_id: string = req.body.product_id;
-//   const store_id: string = req.body.store_id;
-//   const last_modified: string | null = req.body.last_modified;
-//   const restocked: number = req.body.restocked;
-//   const supplier_id: string = req.body.supplier_id;
-//   const notes: string = req.body.notes;
-
-//   // last_modified is equal to kanbe_sell_date in database
-//   // restocked is equal to product_count in database
-//   const queryText: string = `INSERT INTO "incoming_store" ( "user_id", "product_id", "store_id", "kanbe_sell_date", "product_count", "supplier_id", "notes")
-//     VALUES ($1, $2, $3, $4, $5, $6, $7);`;
-//   pool.query(queryText, [user_id, product_id, store_id, last_modified, restocked, supplier_id, notes])
-//     .then(() => res.sendStatus(201))
-//     .catch((err) => {
-//       console.log(`Error saving order to outgoing store: ${err}`);
-//       res.sendStatus(500)
-//     }
-//     );
-
-// });
-
-
-/**
  * POST product to product table
  */
 router.post('/incoming_store', (req: Request, res: Response, next: express.NextFunction): void => {
